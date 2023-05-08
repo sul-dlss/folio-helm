2781cdc2c1a2a15af04e03dcf7788dfd42ca7f0d
Joshua Greben <jgreben@stanford.edu>
2023-05-08 12:23:59 -0700

    Remove onunsed terraform dir

--- terraform/modules/folio-rancher/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "rancher2" {
-  api_url   = var.rancher_server_url
-  token_key = var.rancher_server_token
-}
-
-
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "rancher_server_url" {
-  value       = module.rke.rancher_url
-  description = "Rancher URL"
-}
